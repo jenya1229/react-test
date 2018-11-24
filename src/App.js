46255e3a54a509ac4bdd9eef62489b210fd51010
Evhen Purik <jenyaglor@gmail.com>
2018-11-24 02:09:49 +0200

    добавил удалениею списка по клику кнопки

--- src/App.js
+++ src/App.js
@@ -12,47 +12,81 @@ class App extends Component {
       {name: 'erik', age: 52},
       {name: 'dady', age: 62},
       {name: 'pils', age: 44},
-    ]
+    ],
+    showPerson: false
   }
+
   switchName = () => {
-    if(this.state.persons[0].name === 'max'){
+    
       this.setState({
         persons: [
           {name: 'maximus', age: 28},
-          {name: 'doktor', age: 22},
+          {name:  'wewe', age: 22},
           {name: 'lolypop', age: 32},
           {name: 'erik_swarc', age: 52},
           {name: 'dady_pony', age: 62},
           {name: 'pils_love', age: 44},
         ]
       })
-    }else{
+    }
+
+
+  chengeName = (event) => {
+    
       this.setState({
         persons: [
-          {name: 'max', age: 28},
-          {name: 'dsddd', age: 22},
-          {name: 'lolo', age: 32},
-          {name: 'erik', age: 52},
-          {name: 'dady', age: 62},
-          {name: 'pils', age: 44},
+          {name: 'maximus', age: 28},
+          {name:  'wewe', age: 22},
+          {name:  event.target.value, age: 32},
+          {name: 'erik_swarc', age: 52},
+          {name: 'dady_pony', age: 62},
+          {name: 'pils_love', age: 44},
         ]
       })
-    }
     
-  }
+}
+
+toggleSowDiv = () => {
+  const doesSow = this.state.showPerson;
+  this.setState({showPerson: !doesSow})
+}
   render() {
     // Нет необходимости оборачивать в дополнительный элемент!
+    const style ={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor:'pointer'
+
+
+    }
     return (
        <div className="App">
            <h1> im a cool driver </h1>
            <h1> im a cool driver too </h1>
-           <button onClick={this.switchName}>switch name</button>
-           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-           <Person name={this.state.persons[3].name} age={this.state.persons[3].age}> my hobbies: golf </Person>
-           <Person name={this.state.persons[4].name} age={this.state.persons[4].age}/>
-           <Person name={this.state.persons[5].name} age={this.state.persons[5].age}/>
+
+           <button 
+             style={style}
+             onClick={this.toggleSowDiv}>switch name
+           </button>
+
+        { this.state.showPerson === true ? 
+          <div>
+           <Person 
+             name={this.state.persons[0].name} 
+             age={this.state.persons[0].age}/>
+           <Person 
+             name={this.state.persons[1].name} 
+             age={this.state.persons[1].age}/>
+           <Person 
+             name={this.state.persons[2].name} 
+             age={this.state.persons[2].age}
+             change={this.chengeName}
+             />
+        </div>: null
+      }
+           
        </div>
        
     );
--- src/person/Person.js
+++ src/person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import './Person.css';
 
 const person = (props) => {
 
-  return <div>
-      <p>I'm a {props.name} end i`m {props.age} old !!!</p>
+  return <div className="Person">
+      <p onClick={props.click}> I'm a  {props.name} end i`m {props.age} old !!!> </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.change} />
   </div>
 }
 
